09011cd63cb18ed7aad1ae0b9b76e4eab43b898f
ArmanMotus <arman@datamotus.com>
2023-05-25 15:36:51 +0400

    5/25/2023

--- src/components/UI/blog.jsx
+++ src/components/UI/blog.jsx
@@ -1,22 +1,22 @@
 import React from 'react'
 import '../../styles/blog.css';
 
-
 const blog = () => {
   return (
     <section>
       
-    <div className="container">
-    <div className="blog-container">  
-    <div className="map-container">
+    <div className='blcontainer'>
+    <div >  
+    <div >
       <iframe
         src="https://armandata.shinyapps.io/test3/"
         width="100%"
-        height="450"
+        height="1000"
         frameBorder="0"
         allowFullScreen=""
         aria-hidden="false"
         tabIndex="1"
+        style={{overflow: 'hidden'}}
       ></iframe>
     </div>
     </div>
