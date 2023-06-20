b4877f7bf99f6a1583f287e154ee80b0c9aa05c3
ArmanMotus <arman@datamotus.com>
2023-06-20 14:01:44 +0400

    6/20

--- src/components/UI/blog.jsx
+++ src/components/UI/blog.jsx
@@ -3,26 +3,18 @@ import '../../styles/blog.css';
 
 const blog = () => {
   return (
-    <section>
-      
-    <div >
-    <div >  
-    <div >
-      <iframe
-        src="https://master--merry-lily-bf54d6.netlify.app/"
-        width="100%"
-        height="1000"
-        frameBorder="0"
-        allowFullScreen=""
-        aria-hidden="false"
-        tabIndex="1"
-        style={{overflow: 'hidden'}}
-
-      ></iframe>
-    </div>
-    </div>
-    </div>
-      </section>
+    <div style={{width: "99%", overflow: 'hidden'}}>
+    <iframe
+      src="https://master--merry-lily-bf54d6.netlify.app/"
+      width="101%"
+      height="1000"
+      frameBorder="0"
+      allowFullScreen=""
+      aria-hidden="false"
+      tabIndex="1"
+    ></iframe>
+  </div>
+  
     
   )
 }
