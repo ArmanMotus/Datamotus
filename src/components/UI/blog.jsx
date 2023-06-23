cf2da3224d4febff83f92588d4e4265882ac28dd
ArmanMotus <arman@datamotus.com>
2023-06-23 15:32:40 +0400

    6/23

--- src/components/UI/blog.jsx
+++ src/components/UI/blog.jsx
@@ -1,21 +1,37 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react';
 import '../../styles/blog.css';
 
-const blog = () => {
-  return (
-    <div style={{width: "99%", overflow: 'hidden'}}>
-    <iframe
-      src="https://datamotusblog.netlify.app/"
-      width="101%"
-      height="1000"
-      frameBorder="0"
-      allowFullScreen=""
-      aria-hidden="false"
-      tabIndex="1"
-    ></iframe>
-  </div>
-  
-    
-  )
+const Blog = () => {
+    const iframeRef = useRef(null);
+
+    useEffect(() => {
+        const handleMessage = (event) => {
+            if (typeof event.data === 'object' && event.data.frameHeight) {
+                iframeRef.current.style.height = `${event.data.frameHeight}px`;
+            }
+        }
+
+        window.addEventListener('message', handleMessage);
+        
+        return () => {
+            window.removeEventListener('message', handleMessage);
+        };
+    }, []);
+
+    return (
+        <div style={{width: "100%", overflow: 'hidden'}}>
+            <iframe
+                ref={iframeRef}
+                src="https://datamotusblog.netlify.app/"
+                width="100%"
+                height="1000"
+                frameBorder="0"
+                scrolling="no"
+                allowFullScreen=""
+                aria-hidden="false"
+                tabIndex="1"
+            ></iframe>
+        </div>
+    )
 }
-export default blog;
+export default Blog;
